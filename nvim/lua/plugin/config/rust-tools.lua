497a1b66100fd31dd237796e676a632be7dbad00
zhued <zhufind@yeah.net>
2022-10-24 16:51:02 +0800

    feat: rust-tools: hover_with_actions is deprecated

--- nvim/lua/plugin/config/rust-tools.lua
+++ nvim/lua/plugin/config/rust-tools.lua
@@ -1,7 +1,6 @@
 require('rust-tools').setup({
   tools = { -- rust-tools options
     autoSetHints = true,
-    hover_with_actions = true,
     inlay_hints = {
       show_parameter_hints = false,
       parameter_hints_prefix = "",
