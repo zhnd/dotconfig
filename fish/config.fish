acdc97f1332645feb40c4b9735291ac8903bcf14
zhued <zhufind@yeah.net>
2023-08-26 23:37:51 +0800

    feat: update config for autojump neovim pnpm

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,16 @@ if status is-interactive
     set PATH $HOME/.nvm/versions/node/v18.16.0/bin $PATH
 
     # autojump
-    [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
-
+    if test -f /Users/zhued/.autojump/share/autojump/autojump.fish; . /Users/zhued/.autojump/share/autojump/autojump.fish; end
     # cargo
     set PATH $HOME/.cargo/bin $PATH
+    
+    # neovim
+    set PATH $HOME/develop/tools/nvim-macos/bin $PATH
 end
+# pnpm
+set -gx PNPM_HOME "/Users/zhued/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
